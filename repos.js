36ae61beda939ecba1b4511ce21addd8504d2387
Josh <thisisjoshford@gmail.com>
2023-11-02 22:13:32 -0700

    fix: update and format repo data

--- repos.js
+++ repos.js
@@ -1,26 +1,49 @@
 exports.repos = [
-  { owner: 'near', repo: 'near-api-js' },
-  { owner: 'near', repo: 'near-sdk-js' },
-  { owner: 'near', repo: 'near-sdk-rs' },
-  { owner: 'near', repo: 'wallet-selector' },
+  { owner: 'near', repo: 'docs' },
+  // BOS Gateways (Decentralized Front-ends)
   { owner: 'near', repo: 'near-discovery' },
   { owner: 'near', repo: 'near-discovery-components' },
   { owner: 'nearsocial', repo: 'viewer' },
+
+  // BOS Virtual Machine
   { owner: 'nearsocial', repo: 'vm' },
-  { owner: 'nearsocial', repo: 'social-db' },
-  {owner: 'near', repo: 'near-cli'},
-  {owner: 'near', repo: 'near-cli-rs'},
   { owner: 'near', repo: 'bos-web-engine' },
-  { owner: 'near', repo: 'queryapi' },
-  { owner: 'idos-network', repo: 'idos-sdk-js' },
+
+  // BOS Database
+  { owner: 'nearsocial', repo: 'social-db' },
+
+  // Wallet Integrators
+  { owner: 'near', repo: 'wallet-selector' },
+  { owner: 'blocknative', repo: 'web3-onboard' },
+
+  // FastAuth
   { owner: 'near', repo: 'fast-auth-signer' },
   { owner: 'near', repo: 'mpc-recovery' },
-  {owner: 'near', repo: 'near-vscode'},
-  {owner: 'near', repo: 'create-near-app'},
-  {owner: 'near', repo: 'bos-loader'},
-  {owner: 'bos-cli-rs', repo: 'bos-cli-rs'},
-  {owner: 'keypom', repo: 'keypom'},
-  {owner: 'near', repo: 'near-workspaces-rs'},
-  {owner: 'near', repo: 'near-workspaces-js'},
-  {owner: 'near', repo: 'docs'}
+
+  // idOS (Identity Verification)
+  { owner: 'idos-network', repo: 'idos-sdk-js' },
+
+  // DevTools
+  { owner: 'near', repo: 'create-near-app' },
+  { owner: 'near', repo: 'near-vscode' },
+  { owner: 'near', repo: 'near-cli' },
+  { owner: 'near', repo: 'near-cli-rs' },
+  { owner: 'near', repo: 'bos-loader' },
+  { owner: 'bos-cli-rs', repo: 'bos-cli-rs' },
+
+  // Smart Contracts
+  { owner: 'near', repo: 'near-sdk-js' },
+  { owner: 'near', repo: 'near-sdk-rs' },
+  { owner: 'keypom', repo: 'keypom' },
+
+  // API & Data Indexing
+  { owner: 'near', repo: 'near-api-js' },
+  { owner: 'near', repo: 'queryapi' },
+  { owner: 'near', repo: 'near-lake-indexer' },
+  { owner: 'near', repo: 'near-lake-framework-rs' },
+  { owner: 'near', repo: 'near-lake-framework-js' },
+
+  // Testing
+  { owner: 'near', repo: 'near-workspaces-rs' },
+  { owner: 'near', repo: 'near-workspaces-js' },
 ];
