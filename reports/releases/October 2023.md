7b202dacf8be5eae3679b54c9452e2730bfdbfb2
Josh <thisisjoshford@gmail.com>
2023-11-03 12:56:17 -0700

    chore: move helper functions to utils

---
+++ reports/releases/October 2023.md
@@ -0,0 +1,12 @@
+# NEAR Dev Releases - October 2023 
+
+| owner | repo |
+| --- | --- |
+| nearsocial | vm |
+| near | wallet-selector |
+| blocknative | web3-onboard |
+| near | near-vscode |
+| near | near-cli-rs |
+| near | bos-loader |
+| bos-cli-rs | bos-cli-rs |
+| near | near-workspaces-rs |
---
+++ utils.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const fs = require('fs').promises;
+const axios = require('axios');
+
+const GITHUB_RELEASE_API_URL = 'https://api.github.com/repos/{owner}/{repo}/releases';
+const TOKEN = process.env.GITHUB_TOKEN;
+const HEADERS = {
+  Authorization: `token ${TOKEN}`,
+  Accept: 'application/vnd.github.v3+json',
+};
+
+async function getLatestReleases(owner, repo) {
+  const response = await axios.get(
+    GITHUB_RELEASE_API_URL.replace('{owner}', owner).replace('{repo}', repo),
+    { headers: HEADERS }
+  );
+  const releases = response.data;
+  if (releases.length === 0) {
+    return null;
+  }
+  return new Date(releases[0].published_at);
+}
+
+function generateMarkdown(data, oneMonthAgo) {
+    if (!data.length) {
+      return '# NEAR Dev Report: \n\nNo data available.';
+    }
+  
+    const headers = Object.keys(data[0]);
+    let markdownContent = `# NEAR Dev Releases - ${oneMonthAgo} \n\n`;
+  
+    // Generate headers
+    markdownContent += `| ${headers.join(' | ')} |\n`;
+    // Generate separators
+    markdownContent += `| ${headers.map(() => '---').join(' | ')} |\n`;
+  
+    // Generate table rows
+    for (const item of data) {
+      const row = headers.map((header) => item[header]).join(' | ');
+      markdownContent += `| ${row} |\n`;
+    }
+    return markdownContent;
+  }
+
+async function writeMarkdownFile(filename, content) {
+    await fs.writeFile(filename, content, 'utf8');
+    console.log(`Markdown file '${filename}' has been generated. \n`);
+  }
+
+  module.exports = { generateMarkdown, writeMarkdownFile, getLatestReleases };
