ef4236a548067e327841393be4ba5a4980d4181e
Josh <thisisjoshford@gmail.com>
2024-03-01 10:32:22 -0800

    feat: add descriptions to repos

--- data/repos.js
+++ data/repos.js
@@ -2,63 +2,199 @@ exports.repos = [
   { owner: 'near', repo: 'docs', type: '📄 Documentation' },
 
   // BOS Gateways (Decentralized Front-ends)
-  { owner: 'near', repo: 'near-discovery', type: '🚀 Decentralized Frontends' },
+  {
+    owner: 'near',
+    repo: 'near-discovery',
+    type: '🚀 Decentralized Frontends',
+    desc: 'near.org Decentralized Frontend Gateway',
+  },
   {
     owner: 'near',
     repo: 'near-discovery-components',
     type: '🚀 Decentralized Frontends',
+    desc: 'Core components for near.org Decentralized Frontend Gateway',
   },
-  { owner: 'nearsocial', repo: 'viewer', type: '🚀 Decentralized Frontends' },
+  {
+    owner: 'nearsocial',
+    repo: 'viewer',
+    type: '🚀 Decentralized Frontends',
+    desc: 'near.social Decentralized Frontend Gateway',
+  },
 
   // BOS Virtual Machine
-  { owner: 'nearsocial', repo: 'vm', type: '🚀 Decentralized Frontends' },
-  { owner: 'near', repo: 'bos-web-engine', type: '🚀 Decentralized Frontends' },
+  {
+    owner: 'nearsocial',
+    repo: 'vm',
+    type: '🚀 Decentralized Frontends',
+    desc: 'VM execution layer for NEAR decentralized frontend components ',
+  },
+  {
+    owner: 'near',
+    repo: 'bos-web-engine',
+    type: '🚀 Decentralized Frontends',
+    desc: 'Improved execution layer for NEAR decentralized frontend components',
+  },
 
   // BOS Database
   {
     owner: 'nearsocial',
     repo: 'social-db',
     type: '🚀 Decentralized Frontends',
+    desc: 'Smart contract database for decentralized frontend components & near.social',
   },
 
   // Wallet Integrators
-  { owner: 'near', repo: 'wallet-selector', type: '🔑 Wallet / Auth' },
-  { owner: 'blocknative', repo: 'web3-onboard', type: '🔑 Wallet / Auth' },
+  {
+    owner: 'near',
+    repo: 'wallet-selector',
+    type: '🔑 Wallet / Auth',
+    desc: 'Wallet selector allowing users to interact with NEAR dApps with a selection of available wallets',
+  },
+  {
+    owner: 'blocknative',
+    repo: 'web3-onboard',
+    type: '🔑 Wallet / Auth',
+    desc: 'Wallet selector allowing users to interact with multi-chain apps with a selection of available wallets',
+  },
 
   // FastAuth
-  { owner: 'near', repo: 'fast-auth-signer', type: '🔑 Wallet / Auth' },
-  { owner: 'near', repo: 'mpc-recovery', type: '🔑 Wallet / Auth' },
+  {
+    owner: 'near',
+    repo: 'fast-auth-signer',
+    type: '🔑 Wallet / Auth',
+    desc: 'Fast',
+  },
+  {
+    owner: 'near',
+    repo: 'mpc-recovery',
+    type: '🔑 Wallet / Auth',
+    desc: 'Enables multi-party computation for account creation and recovery',
+  },
 
   // idOS (Identity Verification)
-  { owner: 'idos-network', repo: 'idos-sdk-js', type: '🔑 Wallet / Auth' },
+  {
+    owner: 'idos-network',
+    repo: 'idos-sdk-js',
+    type: '🔑 Wallet / Auth',
+    desc: 'Decentralized identity storage and verification',
+  },
 
   // DevTools
-  { owner: 'near', repo: 'create-near-app', type: '🛠️ Dev Tools' },
-  { owner: 'near', repo: 'near-vscode', type: '🛠️ Dev Tools' },
-  { owner: 'near', repo: 'bos-loader', type: '🛠️ Dev Tools' },
+  {
+    owner: 'near',
+    repo: 'create-near-app',
+    type: '🛠️ Dev Tools',
+    desc: 'Quick scaffolding tool for creating decentralized applications and smart contracts',
+  },
+  {
+    owner: 'near',
+    repo: 'near-vscode',
+    type: '🛠️ Dev Tools',
+    desc: 'VSCode Extension for creating decentralized frontend components on NEAR',
+  },
+  {
+    owner: 'near',
+    repo: 'bos-loader',
+    type: '🛠️ Dev Tools',
+    desc: 'Enables local component development for decentralized frontend components',
+  },
 
-  { owner: 'bos-cli-rs', repo: 'bos-cli-rs', type: '🖥️ CLI' },
-  { owner: 'near', repo: 'near-cli', type: '🖥️ CLI' },
-  { owner: 'near', repo: 'near-cli-rs', type: '🖥️ CLI' },
+  {
+    owner: 'bos-cli-rs',
+    repo: 'bos-cli-rs',
+    type: '🖥️ CLI',
+    desc: 'Command line utility that helps develop components for decentralized frontend components',
+  },
+  {
+    owner: 'near',
+    repo: 'near-cli',
+    type: '🖥️ CLI',
+    desc: 'Command line interface for interacting with NEAR Protocol',
+  },
+  {
+    owner: 'near',
+    repo: 'near-cli-rs',
+    type: '🖥️ CLI',
+    desc: 'A more guided command line interface for interacting with NEAR Protocol (written in Rust) ',
+  },
 
   // Smart Contracts
-  { owner: 'near', repo: 'near-sdk-js', type: '📝 Smart Contracts' },
-  { owner: 'near', repo: 'near-sdk-rs', type: '📝 Smart Contracts' },
-  { owner: 'keypom', repo: 'keypom', type: '📝 Smart Contracts' },
+  {
+    owner: 'near',
+    repo: 'near-sdk-js',
+    type: '📝 Smart Contracts',
+    desc: 'JavaScript SDK for writing smart contracts on NEAR Protocol',
+  },
+  {
+    owner: 'near',
+    repo: 'near-sdk-rs',
+    type: '📝 Smart Contracts',
+    desc: 'Rust SDK for writing smart contracts',
+  },
+  {
+    owner: 'keypom',
+    repo: 'keypom',
+    type: '📝 Smart Contracts',
+    desc: 'Access key factory for creating easy and customizable user onboarding',
+  },
 
   // Testing
-  { owner: 'near', repo: 'near-workspaces-rs', type: '🧪 Testing' },
-  { owner: 'near', repo: 'near-workspaces-js', type: '🧪 Testing' },
+  {
+    owner: 'near',
+    repo: 'near-workspaces-rs',
+    type: '🧪 Testing',
+    desc: 'Sandbox testing environment for NEAR Smart Contracts written in Rust',
+  },
+  {
+    owner: 'near',
+    repo: 'near-workspaces-js',
+    type: '🧪 Testing',
+    desc: 'Sandbox testing environment for NEAR Smart Contracts written in JavaScript',
+  },
 
   // API & Data Indexing
-  { owner: 'near', repo: 'near-api-js', type: '📞 API' },
-  { owner: 'near', repo: 'queryapi', type: '🔎 Data Indexing' },
-  { owner: 'near', repo: 'near-lake-indexer', type: '🔎 Data Indexing' },
-  { owner: 'near', repo: 'near-lake-framework-rs', type: '🔎 Data Indexing' },
-  { owner: 'near', repo: 'near-lake-framework-js', type: '🔎 Data Indexing' },
-  { owner: 'near', repo: 'near-lake-framework-js', type: '🔎 Data Indexing' },
+  {
+    owner: 'near',
+    repo: 'near-api-js',
+    type: '📞 API',
+    desc: 'JavaScript API for interacting with NEAR Protocol',
+  },
+  {
+    owner: 'near',
+    repo: 'queryapi',
+    type: '🔎 Data Indexing',
+    desc: 'Create custom APIs for your dApp using an indexing framework',
+  },
+  {
+    owner: 'near',
+    repo: 'near-lake-indexer',
+    type: '🔎 Data Indexing',
+    desc: 'NEAR Indexer that stores JSON in AWS S3 bucket',
+  },
+  {
+    owner: 'near',
+    repo: 'near-lake-framework-rs',
+    type: '🔎 Data Indexing',
+    desc: 'Stream blocks from NEAR Lake into your server w/ Rust',
+  },
+  {
+    owner: 'near',
+    repo: 'near-lake-framework-js',
+    type: '🔎 Data Indexing',
+    desc: 'Stream blocks from NEAR Lake into your server w/ JavaScript',
+  },
 
   // Protocol
-  { owner: 'near', repo: 'nearcore', type: '⛓️ Protocol' },
-  { owner: 'near', repo: 'neps', type: '⛓️ Protocol' },
+  {
+    owner: 'near',
+    repo: 'nearcore',
+    type: '⛓️ Protocol',
+    desc: 'NEAR Protocol Core',
+  },
+  {
+    owner: 'near',
+    repo: 'neps',
+    type: '⛓️ Protocol',
+    desc: 'NEAR Protocol Enhancement Proposals',
+  },
 ];
